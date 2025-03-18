bb6cfd1bb85e25a431f37c3968648cf794ab62e8
HamedReza97 <95174132+HamedReza97@users.noreply.github.com>
2025-03-18 15:11:19 +0330

    .

--- js/landing.js
+++ js/landing.js
@@ -92,45 +92,46 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function changeTab(index) {
+    if (tabData[index]) {
+        const title = document.getElementById("tab-title");
+        const image = document.getElementById("tab-image");
+        const description = document.getElementById("tab-description");
+        const catButton = document.getElementById("cat-button");
+        // Remove existing animations
+        title.classList.remove("fade-in");
+        image.classList.remove("fade-in");
+        description.classList.remove("fade-in");
+        catButton.classList.remove("fade-in");
 
-    const title = document.getElementById("tab-title");
-    const image = document.getElementById("tab-image");
-    const description = document.getElementById("tab-description");
-    const catButton = document.getElementById("cat-button");
-    // Remove existing animations
-    title.classList.remove("fade-in");
-    image.classList.remove("fade-in");
-    description.classList.remove("fade-in");
-    catButton.classList.remove("fade-in");
+        // Add fade-out animation
+        title.classList.add("fade-out");
+        image.classList.add("fade-out");
+        description.classList.add("fade-out");
+        catButton.classList.add("fade-out");
 
-    // Add fade-out animation
-    title.classList.add("fade-out");
-    image.classList.add("fade-out");
-    description.classList.add("fade-out");
-    catButton.classList.add("fade-out");
+        setTimeout(() => {
+            // Update content
+            title.innerText = tabData[index]['name'];
+            image.src = tabData[index]['image'];
+            description.innerText = tabData[index]['description'];
+            catButton.addEventListener("click", function() {
+                const link = document.createElement("a");
+                link.href = `/products/${tabData[index]['slug']}`;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            });
 
-    setTimeout(() => {
-        // Update content
-        title.innerText = tabData[index]['name'];
-        image.src = tabData[index]['image'];
-        description.innerText = tabData[index]['description'];
-        catButton.addEventListener("click", function() {
-            const link = document.createElement("a");
-            link.href = `/products/${tabData[index]['slug']}`;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        });
+            // Remove fade-out and add fade-in animation
+            title.classList.remove("fade-out");
+            image.classList.remove("fade-out");
+            description.classList.remove("fade-out");
+            catButton.classList.remove("fade-out");
 
-        // Remove fade-out and add fade-in animation
-        title.classList.remove("fade-out");
-        image.classList.remove("fade-out");
-        description.classList.remove("fade-out");
-        catButton.classList.remove("fade-out");
-
-        title.classList.add("fade-in");
-        image.classList.add("fade-in");
-        description.classList.add("fade-in");
-        catButton.classList.add("fade-in");
-    }, 300);
+            title.classList.add("fade-in");
+            image.classList.add("fade-in");
+            description.classList.add("fade-in");
+            catButton.classList.add("fade-in");
+        }, 300);
+    }
 }
